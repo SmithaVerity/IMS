9a5e69cc9a7c1218a42882739f4d564968064ce7
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2022-09-26 02:05:05 +0530

    Update constants.php

--- inc/config/constants.php
+++ inc/config/constants.php
@@ -5,10 +5,10 @@
 	
 	// Database parameters
 	// Data source name
-	define('DSN', 'mysql:host=imsdb;dbname=shop_inventory');
+	define('DSN', 'mysql:host=localhost;dbname=shop_inventory');
 	
 	// Hostname
-	define('DB_HOST', 'imsdb');
+	define('DB_HOST', 'localhost');
 	
 	// DB user
 	define('DB_USER', 'ubuntu');
