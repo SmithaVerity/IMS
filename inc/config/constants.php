f35ed2648194219d18f0c554d5d8e0263e22cf72
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2022-09-26 12:19:04 +0530

    Update constants.php

--- inc/config/constants.php
+++ inc/config/constants.php
@@ -1,20 +1,20 @@
 <?php
 	// Root url for the site
-	define('ROOT_URL', 'http://34.208.162.213/IMS');
+	define('ROOT_URL', 'http://54.255.178.159/IMS');
 	
 	
 	// Database parameters
 	// Data source name
-	define('DSN', 'mysql:host=localhost;dbname=shop_inventory');
+	define('DSN', 'mysql:host=migrationtest.cjerdsoatzri.ap-southeast-1.rds.amazonaws.com;dbname=shop_inventory');
 	
 	// Hostname
-	define('DB_HOST', 'localhost');
+	define('DB_HOST', 'migrationtest.cjerdsoatzri.ap-southeast-1.rds.amazonaws.com');
 	
 	// DB user
-	define('DB_USER', 'ubuntu');
+	define('DB_USER', 'admin');
 	
 	// DB password
-	define('DB_PASSWORD', 'Ubuntu@123');
+	define('DB_PASSWORD', 'admin123');
 	
 	// DB name
 	define('DB_NAME', 'shop_inventory');
